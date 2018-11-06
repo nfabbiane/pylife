1bb8164c7e6b295c017162a339dc481d24fcf467
Nicolò Fabbiane <nfabbiane@gmail.com>
2018-11-06 22:19:02 +0100

    fix for pytest

--- test/test.py
+++ test/test.py
@@ -19,28 +19,22 @@ import pylife
 ################################################################################
 # Define square steady solution check
 #
-def check_square(steps):
+def check_square(steps=10):
 	# initialize
 	map = pylife.map(shape=[3, 3])
-	# initialize plot
-	plt = pylife.plot.lite(); plt.open()
 	# steady state
 	square=[[1, 1, 0],
 			  [1, 1, 0],
 			  [0, 0, 0]]
 	# initialize state
 	map.setstate(square)
-	plt.plot(map)
 	# - check
 	passed=(square==map.getstate()).all()
 	# step state
 	for i in range(steps):
 		map.step()
-		plt.plot(map, step=i+1)
 		# check steady state
 		passed=passed&(square==map.getstate()).all()
-	# close plot session
-	plt.close()
 	# output
 	assert(passed)
 
@@ -50,11 +44,9 @@ def check_square(steps):
 ################################################################################
 # Define propeller periodic orbit check
 #
-def check_propeller(steps):
+def check_propeller(steps=10):
 	# initialize
 	map = pylife.map(shape=[3, 3])
-	# initialize plot
-	plt = pylife.plot.lite(); plt.open()
 	# states of the period 2 orbit
 	propeller=[[[0, 0, 0],
 					[1, 1, 1],
@@ -64,17 +56,13 @@ def check_propeller(steps):
 					[0, 1, 0]]]
 	# initialize state
 	map.setstate(propeller[0])
-	plt.plot(map)
 	# - check
 	passed=(propeller[0]==map.getstate()).all()
 	# step state
 	for i in range(steps):
 		map.step()
-		plt.plot(map, step=i+1)
 		# check periodic orbit
 		passed=passed&(propeller[(i+1)%2]==map.getstate()).all()
-	# close plot session
-	plt.close()
 	# output
 	assert(passed)
 
@@ -87,8 +75,6 @@ def check_propeller(steps):
 def test_glider(steps=20):
 	# initialize
 	map = pylife.map(shape=[5, 5], bc=["periodic", "periodic"])
-	# initialize plot
-	plt = pylife.plot.lite(); plt.open()
 	# initial state
 	glider=[[1, 0, 0, 0, 0],
 			  [0, 1, 1, 0, 0],
@@ -97,19 +83,36 @@ def test_glider(steps=20):
 			  [0, 0, 0, 0, 0]]
 	# initialize state
 	map.setstate(glider)
-	plt.plot(map)
 	# - check
 	passed=(glider==map.getstate()).all()
 	# step state
 	for i in range(0, steps):
 		map.step()
-		plt.plot(map, step=i+1)
 		# check glider
 		if (i+1)%20==0: passed=passed&(glider==map.getstate()).all()
-	# close plot session
-	plt.close()
 	# output
 	assert(passed)
+
+
+
+
+################################################################################
+# Define propeller periodic orbit check
+#
+def check_plot_lite(steps=10):
+	# initialize
+	map = pylife.map(shape=[3, 3])
+	# initialize plot
+	plt = pylife.plot.lite(); plt.open()
+	# initialize state (random)
+	map.rand()
+	plt.plot(map, step=0)
+	# step state
+	for i in range(steps):
+		map.step()
+		plt.plot(map, step=i+1)
+	# close plot session
+	plt.close()
 
 
 
